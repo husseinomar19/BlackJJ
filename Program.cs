3d2fd423654f7b901b7e410833ac9b6470e4509b
hussein <118814567+husseinomar19@users.noreply.github.com>
2024-03-01 09:55:53 +0100

    Inti

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace BlackJJ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!"); 
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
